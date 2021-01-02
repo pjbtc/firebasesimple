8fa41ccbffe980d2a9398a65895057321f640323
pjbtc <pjbhattachan@gmail.com>
2021-01-02 22:30:13 +0545

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const DB_config = {
+  apiKey: "AIzaSyB174e-dOCRwJbyoevYpH3_DACSWDYdA_w",
+  authDomain: "newtodo-64b4e.firebaseapp.com",
+  projectId: "newtodo-64b4e",
+  storageBucket: "newtodo-64b4e.appspot.com",
+  messagingSenderId: "364444160625",
+  appId: "1:364444160625:web:ce67cb340bb892518681d0"
+};
